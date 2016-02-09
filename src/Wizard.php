2bdf09e1b05a6bccf60751c7828f2e4c2fbfcaf5
Sebastian Bergmann <sb@sebastian-bergmann.de>
2016-02-09 04:00:52 -0500

    Allow resolving code units that are loaded after the first call to lookup()

--- src/Wizard.php
+++ src/Wizard.php
@@ -18,7 +18,17 @@ class Wizard
     /**
      * @var array
      */
-    private $lookupTable;
+    private $lookupTable = [];
+
+    /**
+     * @var array
+     */
+    private $processedClasses = [];
+
+    /**
+     * @var array
+     */
+    private $processedFunctions = [];
 
     /**
      * @param string $filename
@@ -28,7 +38,9 @@ class Wizard
      */
     public function lookup($filename, $lineNumber)
     {
-        $this->initLookupTable();
+        if (!isset($this->lookupTable[$filename][$lineNumber])) {
+            $this->updateLookupTable();
+        }
 
         if (isset($this->lookupTable[$filename][$lineNumber])) {
             return $this->lookupTable[$filename][$lineNumber];
@@ -37,14 +49,8 @@ class Wizard
         }
     }
 
-    private function initLookupTable()
+    private function updateLookupTable()
     {
-        if ($this->lookupTable !== null) {
-            return;
-        }
-
-        $this->lookupTable = [];
-
         $this->processClassesAndTraits();
         $this->processFunctions();
     }
@@ -52,18 +58,30 @@ class Wizard
     private function processClassesAndTraits()
     {
         foreach (array_merge(get_declared_classes(), get_declared_traits()) as $classOrTrait) {
+            if (isset($this->processedClasses[$classOrTrait])) {
+                continue;
+            }
+
             $reflector = new \ReflectionClass($classOrTrait);
 
             foreach ($reflector->getMethods() as $method) {
                 $this->processFunctionOrMethod($method);
             }
+
+            $this->processedClasses[$classOrTrait] = true;
         }
     }
 
     private function processFunctions()
     {
         foreach (get_defined_functions()['user'] as $function) {
+            if (isset($this->processedFunctions[$function])) {
+                continue;
+            }
+
             $this->processFunctionOrMethod(new \ReflectionFunction($function));
+
+            $this->processedFunctions[$function] = true;
         }
     }
 
